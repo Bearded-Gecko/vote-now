724c15592258a265a9b5de0f693e5c99efbfbebe
Mike_Ho_G15 <michaelyho1@gmail.com>
2023-05-18 14:07:59 -0700

    deleted redundant poll_project folder

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-#import 
+#import environment variables to send email
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
